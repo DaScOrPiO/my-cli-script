a2b881fb3f9c1e94fdabd40aeb6ee2fe9caf58dd
DaScOrPiO <dunni82@yahoo.com>
2023-06-06 17:37:16 +0100

    FEAT: Added copy Feature

--- node-practice.js
+++ node-practice.js
@@ -8,7 +8,7 @@ let exitFlag = false;
 
 while (!exitFlag) {
   const firstAsk = prompt(
-    "What would you like to do? (Type 'F' for Folders, 'f' for files) Exit with 'Q'): "
+    "What would you like to do? (Type 'F' for Folders, 'f' for files, 'C' to copy) Exit with 'Q'): "
   );
 
   //Create Folder
@@ -68,6 +68,32 @@ while (!exitFlag) {
     //Terminates Operation
     console.log("Process Terminated");
     exitFlag = true;
+  } else if (firstAsk === "C") {
+    // Copy files
+    let inputpath = "";
+    let inputDestination = "";
+    const copyfiles = (sourcePath, destination) => {
+      if (inputpath !== "X") {
+        try {
+          inputpath = prompt("Enter path to file (type X to exit): ");
+          if (inputpath !== "X") {
+            sourcePath = inputpath;
+            const fileData = fs.readFileSync(sourcePath, "utf-8");
+            console.log(fileData);
+            inputDestination = prompt(
+              "Enter path to directory (type X to exit): "
+            );
+            if (inputDestination !== "X") {
+              destination = inputDestination;
+              fs.writeFileSync(destination, fileData);
+            }
+          }
+        } catch (err) {
+          console.log(`Something Went Wrong: ${err}`);
+        }
+      }
+    };
+    copyfiles(inputpath, inputDestination);
   } else {
     console.log("Invalid input!");
   }
